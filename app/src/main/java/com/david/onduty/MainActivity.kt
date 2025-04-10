3d61a2d19066ecfe89aabb0f363c6666fcbcbd57
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2025-04-10 11:29:22 +0100

    #1, Add event icons for each day
    Added section within cell layout to show event icons,
    and style layouts.

--- app/src/main/java/com/david/onduty/MainActivity.kt
+++ app/src/main/java/com/david/onduty/MainActivity.kt
@@ -1,22 +1,38 @@
 package com.david.onduty
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.david.onduty.ui.CalendarAdapter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var calendarView: RecyclerView
+    private lateinit var monthView: TextView
+    private lateinit var date: LocalDate
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dataSet = arrayOf(1, 2, 3)
-        val adapter = CalendarAdapter(dataSet)
-
-        val recyclerView: RecyclerView = findViewById(R.id.calendarRecyclerView)
-        recyclerView.layoutManager = GridLayoutManager(this, 7)
-        recyclerView.adapter = adapter
-
+        date = LocalDate.now()
+        initViews();
     }
+
+    private fun initViews() {
+        monthView = findViewById(R.id.monthYearTextView)
+        monthView.text = date.format(DateTimeFormatter.ofPattern("MMMM yyyy"))
+
+        val dataSet = IntArray(42) {i -> i + 1}
+        val adapter = CalendarAdapter(dataSet.toTypedArray())
+
+        calendarView = findViewById(R.id.calendarRecyclerView)
+        calendarView.layoutManager = GridLayoutManager(this, 7)
+        calendarView.adapter = adapter
+    }
+
 }
--- app/src/main/java/com/david/onduty/ui/CalendarAdapter.kt
+++ app/src/main/java/com/david/onduty/ui/CalendarAdapter.kt
@@ -1,21 +1,26 @@
 package com.david.onduty.ui
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.david.onduty.R
+import kotlin.random.Random
 
 class CalendarAdapter(private val dataSet: Array<Int>) : RecyclerView.Adapter<CalendarAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val dayView: TextView
+        val eventsDotsLayout: LinearLayout
 
         init {
             dayView = view.findViewById(R.id.dayView)
+            eventsDotsLayout = view.findViewById(R.id.eventDotsLayout)
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,9 +31,29 @@ class CalendarAdapter(private val dataSet: Array<Int>) : RecyclerView.Adapter<Ca
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.dayView.text = dataSet[position].toString()
+
+        for (i in 1..Random.nextInt(1, 5)) {
+
+            if (i == 4) {
+                val eventPlusView = ImageView(holder.itemView.context).apply {
+                    setImageResource(R.drawable.event_plus)
+                }
+
+                holder.eventsDotsLayout.addView(eventPlusView)
+                break
+            } else {
+                val eventDotView = ImageView(holder.itemView.context).apply {
+                    setImageResource(R.drawable.event_dot)
+                    setColorFilter(Color.BLUE)
+                }
+
+                holder.eventsDotsLayout.addView(eventDotView)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         return dataSet.size
     }
+
 }
